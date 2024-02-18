84b78a7941e62f554a97e166aed9c0eaedeb0a76
hrKia <hir.ki.ame.am@gmail.com>
2024-02-19 01:17:00 +0900

    categoryFilter

--- components/catSelector.vue
+++ components/catSelector.vue
@@ -1,146 +1,219 @@
 <template>
   <div>
-    <p>selectedLine:{{ selectedLine }}</p>
-    <p>inputtest: {{ input || 'notInput' }}</p>
-    <p>selecteDItem: {{ selectedItem }}</p>
-    <div class="searchBox" v-if="searchBox">
-      <input id="searchInput" type="search" v-model="input">
-      <label for="searchInput">検索条件を入力</label>
+    <div>
+      <p>input item</p>
+      <input type="text" v-model="input">
+      <button @click="() => {
+        resetValues()
+      }">reset</button>
     </div>
-    <div class="chips">
-      <span class="chip" v-for="category in categoryList" :key="categoryList" @click="() => {
-        selectedLine = category
-        selectedItem = ''
-      }" :class="{ selected: selectedLine === category }">
-        {{ category }}
-      </span>
+
+    <p>line</p>
+    <div class="selectContainer">
+      <div v-for="content in lineList" :key="content" class="content" :class="{ active: selectedLine === content }"
+        @click="() => {
+          selectedLine = selectedLine === content ? '' : content
+          selectedSubLine = ''
+          selectedItem = ''
+          selectItem()
+        }
+          " :style="`min-width:25%;`">
+        <p>{{ content }}</p>
+      </div>
     </div>
-    <div class="sublinechips">
-      <span class="chip" v-for="subline in filteredSubline" :key="subline.subline" @click="() => {
-        selectedItem = subline.subline
-      }" :class="{ selected: selectedItem === subline.subline }">
-        {{ subline.subline }}
-      </span>
+    <p>subline</p>
+    <div class="selectContainer">
+      <div v-for="content in subLineList" :key="content" class="content" :class="{ active: selectedSubLine === content }"
+        @click="() => {
+          // selectedLine = categoryList.filter((itm) => {
+          //   return itm.Subline === content
+          // })[0].line
+          selectedLine = cattegoryFilter('Subline', 'line', content)
+          selectedSubLine = selectedSubLine === content ? '' : content
+          selectedItem = ''
+          selectItem()
+        }
+          " :style="`min-width:25%;`">
+        <p>{{ content }}</p>
+      </div>
     </div>
+    <p>item</p>
+    <div class="selectContainer">
+      <div v-for="content in itemList" :key="content" class="content" :class="{ active: selectedItem === content }"
+        @click="() => {
+          // selectedLine = categoryList.filter((itm) => {
+          //   return itm.Item === content
+          // })[0].line
+          // selectedSubLine = categoryList.filter((itm) => {
+          //   return itm.Item === content
+          // })[0].Subline
+          selectedLine = cattegoryFilter('Item', 'line', content)
+          selectedSubLine = cattegoryFilter('Item', 'Subline', content)
+          selectedItem = selectedItem === content ? '' : content
+          selectItem()
+        }
+          " :style="`min-width:25%;`">
+        <p>{{ content }}</p>
+      </div>
+    </div>
+    {{ resultValue }}
   </div>
 </template>
 
 <script setup lang="ts">
-
-const props = defineProps({
-  categoryList: {
-    type: Array,
-    default: ['Beauty', 'Variety', 'Stationary', 'Interior', 'all']
-  },
-  searchBox: {
-    type: Boolean,
-    default: true
-  }
-})
-const childItems = [
-  { line: 'Beauty', subline: 'b1' },
-  { line: 'Beauty', subline: 'b2' },
-  { line: 'Beauty', subline: 'b3' },
-  { line: 'Beauty', subline: 'b4' },
-  { line: 'Beauty', subline: 'b5' },
-  { line: 'Variety', subline: 'v1' },
-  { line: 'Variety', subline: 'v2' },
-  { line: 'Variety', subline: 'v3' },
-  { line: 'Variety', subline: 'v4' },
-  { line: 'Variety', subline: 'v5' },
-  { line: 'Stationary', subline: 's1' },
-  { line: 'Stationary', subline: 's2' },
-  { line: 'Stationary', subline: 's3' },
-  { line: 'Stationary', subline: 's4' },
-  { line: 'Stationary', subline: 's5' },
-  { line: 'Interior', subline: 'i1' },
-  { line: 'Interior', subline: 'i2' },
-  { line: 'Interior', subline: 'i3' },
-  { line: 'Interior', subline: 'i4' },
-  { line: 'Interior', subline: 'i5' },
+const categoryList = [
+  { line: 1, Subline: 10, Item: 1000 },
+  { line: 1, Subline: 10, Item: 1001 },
+  { line: 1, Subline: 11, Item: 1002 },
+  { line: 1, Subline: 11, Item: 1003 },
+  { line: 1, Subline: 11, Item: 1004 },
+  { line: 1, Subline: 12, Item: 1005 },
+  { line: 1, Subline: 12, Item: 1006 },
+  { line: 1, Subline: 12, Item: 1007 },
+  { line: 1, Subline: 12, Item: 1008 },
+  { line: 1, Subline: 12, Item: 1009 },
+  { line: 2, Subline: 20, Item: 2000 },
+  { line: 2, Subline: 20, Item: 2001 },
+  { line: 2, Subline: 21, Item: 2002 },
+  { line: 2, Subline: 21, Item: 2003 },
+  { line: 2, Subline: 21, Item: 2004 },
+  { line: 2, Subline: 22, Item: 2005 },
+  { line: 2, Subline: 22, Item: 2006 },
+  { line: 2, Subline: 22, Item: 2007 },
+  { line: 2, Subline: 22, Item: 2008 },
+  { line: 2, Subline: 22, Item: 2009 },
+  { line: 4, Subline: 40, Item: 4000 },
+  { line: 4, Subline: 40, Item: 4001 },
+  { line: 4, Subline: 41, Item: 4002 },
+  { line: 4, Subline: 41, Item: 4003 },
+  { line: 4, Subline: 41, Item: 4004 },
+  { line: 4, Subline: 42, Item: 4005 },
+  { line: 4, Subline: 42, Item: 4006 },
+  { line: 4, Subline: 42, Item: 4007 },
+  { line: 4, Subline: 42, Item: 4008 },
+  { line: 4, Subline: 42, Item: 4009 },
+  { line: 3, Subline: 30, Item: 3000 },
+  { line: 3, Subline: 30, Item: 3001 },
+  { line: 3, Subline: 31, Item: 3002 },
+  { line: 3, Subline: 31, Item: 3003 },
+  { line: 3, Subline: 31, Item: 3004 },
+  { line: 3, Subline: 32, Item: 3005 },
+  { line: 3, Subline: 32, Item: 3006 },
+  { line: 3, Subline: 32, Item: 3007 },
+  { line: 3, Subline: 32, Item: 3008 },
+  { line: 3, Subline: 32, Item: 3009 },
 ]
-const selectedLine: any = ref('all')
-const filteredSubline = computed(() => {
-  let firstResult = []
-  if (selectedLine.value === 'all') {
-    firstResult = childItems
-  } else {
-    firstResult = childItems.filter((item) => {
-      return item.line === selectedLine.value
-    })
+const cattegoryFilter = (level, resultLevel, key) => {
+  return categoryList.filter((itm) => {
+    return itm[level] === key
+  })[0][resultLevel]
+}
+const input = ref()
+const selectedLine = ref()
+const selectedSubLine = ref()
+const selectedItem = ref()
+const resultValue = computed(() => {
+  return {
+    line: selectedLine.value,
+    subline: selectedSubLine.value,
+    item: selectedItem.value
   }
-  firstResult = firstResult.filter((item) => {
-    return item.subline.includes(input.value)
-  })
-  if (firstResult.length === 1) {
-    selectedItem.value = firstResult[0].subline
-  }
-  return firstResult
 })
-const input = ref('')
-const selectedItem = ref('')
+
+const objToList = (obj: any, key: string) => {
+  return Array.from(new Set(obj.map(itm => itm[key]))).sort()
+}
+
+const lineList = computed(() => {
+  return objToList(categoryList, 'line')
+})
+const subLineList = computed(() => {
+  let filterList = categoryList.filter((itm) => {
+    if (selectedLine.value) {
+      return itm.line === selectedLine.value
+    } else {
+      return true
+    }
+  })
+  return objToList(filterList, 'Subline')
+})
+const itemList = computed(() => {
+
+  let filterList = categoryList.filter((itm) => {
+    if (input.value) {
+      return itm.Item == input.value
+    } else {
+      return true
+    }
+  })
+  if (filterList.length === 1) {
+    selectedItem.value = filterList[0].Item
+    selectedLine.value = filterList[0].line
+    selectedSubLine.value = filterList[0].Subline
+    selectItem()
+  } else if (filterList.length === 0) {
+    selectedItem.value = ''
+  }
+  filterList = filterList.filter((itm) => {
+    if (selectedSubLine.value) {
+      return (itm.line == selectedLine.value && itm.Subline === selectedSubLine.value)
+    } else {
+      return true
+    }
+  })
+  return objToList(filterList, 'Item')
+})
+
+const resetValues = () => {
+  selectedLine.value = ''
+  selectedSubLine.value = ''
+  selectedItem.value = ''
+}
+
+// const itemWidth = computed(() => {
+//   if (props.rowItemCount === 0) {
+//     return '25%'
+//   }
+//   else if (props.rowItemCount > props.contentList.length) {
+//     return `${100 / props.contentList.length}%`
+//   } else {
+//     return `${100 / props.rowItemCount}%`
+//   }
+// })
+
+const emits = defineEmits<{
+  (e: 'selected', result: Object): void
+}>()
+
+const selectItem = () => {
+  emits('selected', resultValue.value)
+}
 
 </script>
 <style scoped>
 * {
-  font-family: 'poppins';
+  box-sizing: border-box;
 }
 
-.chips {
+.selectContainer {
+  padding: 5px 0;
   display: flex;
   flex-wrap: wrap;
-  justify-content: space-around;
+  gap: 2px 0;
+  width: 100%;
 }
 
-.chip.selected {
-  background-color: skyblue;
-}
-
-.sublinechips {
-  justify-content: flex-start;
-
-}
-
-.sublinechips .chip {
-  display: inline-block;
-  min-width: 100px;
-  padding: 2px 10px;
-  margin: 2px 5px;
+.content {
+  padding: 2px 5px;
+  min-width: 25%;
   text-align: center;
-  border-radius: 10px;
-  outline: 1px solid black;
+  background-color: lightgray;
+  color: gray;
+  cursor: pointer;
 }
 
-input {
-  padding: 15px;
-  outline: none;
-  border-style: none;
-  font-weight: normal;
-  font-size: 1.2em;
-  border-bottom: 3px solid lightslategray;
-  width: 30vw;
-  background-color: ghostwhite;
-  transition: all .3s;
-}
-
-input:focus {
-  border-bottom: 3px solid teal;
-  background-color: aliceblue;
-  transform: translateY(10px);
-  padding-top: 30px;
-}
-
-.searchBox {
-  position: relative;
-}
-
-.searchBox label {
-  position: absolute;
-  left: 7px;
-  top: 25%;
-}
-
-.searchBox input:focus~label .searchBox :valid:focus~label {
-  transform: translate(-7px) scale(.8);
+.content.active {
+  background-color: teal;
+  color: white;
 }
 </style>
---
+++ pages/compdebug.vue
@@ -0,0 +1,19 @@
+
+import type selectsVue from '~/components/selects.vue';
+<template>
+  <div class="catSelector">
+    <CatSelector @selected="selectedValue" />
+  </div>
+</template>
+
+<script setup>
+const selectedValue = (e) => {
+  console.log(e.item || e.subline || e.line)
+}
+</script>
+
+<style scoped>
+.catSelector {
+  width: 100%;
+}
+</style>
